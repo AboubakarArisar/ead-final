052a7401283f269250bc308274bdeda86e828022
AboubakarArisar <aboubakar.bscsf22@iba-suk.edu.pk>
2025-06-03 11:21:31 +0500

    made react functional component

--- TrafficLightSimulator/src/TrafficSimulator.jsx
+++ TrafficLightSimulator/src/TrafficSimulator.jsx
@@ -3,7 +3,16 @@ import React from "react";
 const TrafficSimulator = () => {
   return (
     <>
-      <h1 className='bg-black text-white'>Traffic simulator</h1>
+      <div className='bg-gray-900 min-h-screen w-full flex justify-center items-center'>
+        <div className='h-[50vh] w-[50%]  shadow-lg flex flex-col justify-center gap-4 items-center'>
+          <h4 className='text-white text-normal'>Traffic Light Simulator</h4>
+          <div className='h-[30vh] w-[13%] bg-gray-800 rounded-lg shadow-lg py-2 px-2 flex flex-col justify-center items-center gap-4'>
+            <div className='w-[50px] h-[50px] rounded-full bg-gray-600 shadow'></div>
+            <div className='w-[50px] h-[50px] rounded-full bg-gray-600 shadow'></div>
+            <div className='w-[50px] h-[50px] rounded-full bg-gray-600 shadow'></div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
